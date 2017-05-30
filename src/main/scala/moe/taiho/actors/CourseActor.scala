1fa11d57d6c5c399e5e8b675e1346c251450f175
Jianfeng Zhang <swordfeng123@gmail.com>
2017-05-31 01:24:12 +0800

    add student and course actor

--- src/main/scala/moe/taiho/actors/CourseActor.scala
+++ src/main/scala/moe/taiho/actors/CourseActor.scala
@@ -1,7 +1,85 @@
 package moe.taiho.actors
 
-import akka.persistence.PersistentActor
+import akka.cluster.sharding.ShardRegion
+import akka.event.Logging
+import akka.persistence.{PersistentActor, SnapshotOffer}
+
+import scala.collection.mutable
+
+object CourseActor {
+
+    sealed trait Command
+    case class SetLimit(num: Int) extends Command
+    case class Take(student: Int, deliveryId: Long) extends Command
+    case class Drop(student: Int, deliveryId: Long) extends Command
+
+    case class Envelope(id: Int, command: Command)
+
+    val extractEntityId: ShardRegion.ExtractEntityId = {
+        case Envelope(id: Int, command: Command) => (id.toString, command)
+    }
+    def extractShardId(numShards: Int): ShardRegion.ExtractShardId = {
+        case m: Envelope => (m.id.hashCode % numShards).toString
+        case ShardRegion.StartEntity(id) => (id.toInt.hashCode % numShards).toString
+    }
+}
 
 class CourseActor extends PersistentActor {
+    import CourseActor._
 
+    val id = self.path.name.toInt
+
+    val log = Logging(context.system, this)
+
+    class State {
+        private var limit = 0
+        private var numSelected = 0
+        private val selected: mutable.TreeMap[Int, (Boolean, Long)] = mutable.TreeMap()
+
+        def hasStudent(student: Int): Boolean = selected contains student
+        def isFull: Boolean = numSelected >= limit
+        def newer(student: Int, deliveryId: Long): Boolean = !hasStudent(student) || selected(student)._2 < deliveryId
+
+        def update(m: Command): Unit = {
+            m match {
+                case SetLimit(num) => limit = num
+                case Take(student, deliveryId) => {
+                    assert(newer(student, deliveryId))
+                    selected(student) = (true, deliveryId)
+                }
+                case Drop(student, deliveryId) => {
+                    assert(newer(student, deliveryId))
+                    selected(student) = (false, deliveryId)
+                }
+            }
+        }
+    }
+
+    var state: State = new State()
+
+    override def receiveRecover: Receive = {
+        case m: Command => state.update(m)
+    }
+
+    override def receiveCommand: Receive = {
+        case m @ Take(student, deliveryId) => {
+            if (state.newer(student, deliveryId)) {
+                if (!state.isFull) persist(m) { m =>
+                    state.update(m)
+                    sender() ! StudentActor.Envelope(student, StudentActor.Taken(id, deliveryId))
+                } else {
+                    sender() ! StudentActor.Envelope(student, StudentActor.Full(id, deliveryId))
+                }
+            }
+        }
+        case m @ Drop(student, deliveryId) => {
+            if (state.newer(student, deliveryId)) persist(m) { m =>
+                state.update(m)
+                sender() ! StudentActor.Envelope(student, StudentActor.Dropped(id, deliveryId))
+            }
+        }
+        case _ => log.info(s"unhandled message on Course $id")
+    }
+
+    override def persistenceId: String = s"Course-$id"
 }
--- src/main/scala/moe/taiho/actors/StudentActor.scala
+++ src/main/scala/moe/taiho/actors/StudentActor.scala
@@ -1,6 +1,138 @@
 package moe.taiho.actors
 
-import akka.persistence.PersistentActor
+import akka.actor.ActorRef
+import akka.cluster.sharding.ShardRegion
+import akka.event.Logging
+import akka.persistence.{AtLeastOnceDelivery, PersistentActor}
 
-class StudentActor extends PersistentActor {
+import scala.collection.mutable
+
+object StudentActor {
+    sealed trait Command
+    // Requested by frontend
+    case class Take(course: Int) extends Command
+    case class Drop(course: Int) extends Command
+    // Course responses
+    case class Taken(course: Int, deliveryId: Long) extends Command
+    case class Full(course: Int, deliveryId: Long) extends Command
+    case class Dropped(course: Int, deliveryId: Long) extends Command
+
+    case class Envelope(id: Int, command: Command)
+
+    // reply to frontend
+    case class Success(student: Int, course: Int, target: Boolean)
+    case class Failure(student: Int, course: Int, target: Boolean, reason: String)
+
+    val extractEntityId: ShardRegion.ExtractEntityId = {
+        case Envelope(id: Int, command: Command) => (id.toString, command)
+    }
+    def extractShardId(numShards: Int): ShardRegion.ExtractShardId = {
+        case m: Envelope => (m.id.hashCode % numShards).toString
+        case ShardRegion.StartEntity(id) => (id.toInt.hashCode % numShards).toString
+    }
+}
+
+class StudentActor extends PersistentActor with AtLeastOnceDelivery {
+    import StudentActor._
+
+    val log = Logging(context.system, this)
+
+    val id: Int = self.path.name.toInt
+
+    class State {
+        private val selected: mutable.Map[Int, (Boolean/*target*/, Boolean/*confirmed*/, Long)] = mutable.TreeMap()
+
+        def effective(course: Int, deliveryId: Long): Boolean = {
+            selected get course exists { t =>
+                assert(deliveryId <= t._3)
+                !t._2 && deliveryId == t._3
+            }
+        }
+
+        def update(m: Command): Unit = {
+            m match {
+                case Taken(course, deliveryId) =>
+                    assert(effective(course, deliveryId))
+                    confirmDelivery(deliveryId)
+                    selected(course) = (true, true, deliveryId)
+                case Full(course, deliveryId) =>
+                    assert(effective(course, deliveryId))
+                    confirmDelivery(deliveryId)
+                    selected remove course
+                case Dropped(course, deliveryId) =>
+                    assert(effective(course, deliveryId))
+                    confirmDelivery(deliveryId)
+                    selected.remove(course)
+                case Take(course) => deliver(context.actorSelection(s"/user/Course")) {
+                    deliveryId =>
+                        selected get course foreach { t => if (!t._2) confirmDelivery(t._3) }
+                        selected(course) = (true, false, deliveryId)
+                        CourseActor.Envelope(course, CourseActor.Take(id, deliveryId))
+                }
+                case Drop(course) => deliver(context.actorSelection(s"/user/Course")) {
+                    deliveryId =>
+                        selected get course foreach { t => if (!t._2) confirmDelivery(t._3) }
+                        selected(course) = (false, false, deliveryId)
+                        CourseActor.Envelope(course, CourseActor.Drop(id, deliveryId))
+                }
+            }
+        }
+
+        def query(course: Int): (Boolean, Boolean) = selected get course map { t => (t._1, t._2) } getOrElse (false, true)
+    }
+
+    var state = new State()
+
+    val requests: mutable.Map[Int, ActorRef] = mutable.TreeMap()
+
+    override def receiveRecover: Receive = {
+        case m: Command => state.update(m)
+    }
+
+    override def receiveCommand: Receive = {
+        case m @ Taken(course, deliveryId) =>
+            if (state.effective(course, deliveryId)) persist(m) { m =>
+                state.update(m)
+                requests get course foreach { s =>
+                    s ! Success(id, course, true)
+                    requests remove course
+                }
+            }
+        case m @ Full(course, deliveryId) =>
+            if (state.effective(course, deliveryId)) persist(m) { m =>
+                state.update(m)
+                requests get course foreach { s =>
+                    s ! Success(id, course, true)
+                    requests remove course
+                }
+            }
+        case m @ Dropped(course, deliveryId) =>
+            if (state.effective(course, deliveryId)) persist(m) { m =>
+                state.update(m)
+                requests get course foreach { s =>
+                    s ! Success(id, course, false)
+                    requests remove course
+                }
+            }
+        case m @ Take(course) =>
+            state.query(course) match {
+                case (true, false) => // do nothing
+                case (true, true) => sender() ! Success(id, course, true)
+                case _ => persist(m) { m =>
+                    requests(course) = sender()
+                    state.update(m)
+                }
+            }
+        case m @ Drop(course) =>
+            state.query(course) match {
+                case (false, false) => // do nothing
+                case (false, true) => sender() ! Success(id, course, false)
+                case _ => persist(m) { m =>
+                    requests(course) = sender()
+                    state.update(m)
+                }
+            }
+    }
+
+    override def persistenceId: String = s"Student-$id"
 }
