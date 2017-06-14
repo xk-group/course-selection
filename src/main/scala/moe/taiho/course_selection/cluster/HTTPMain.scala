a3386e6f1ecda5717fa3e86c24ef91be74965631
crazyboycjr <crazyboycjr@gmail.com>
2017-06-14 19:31:48 +0800

    add bind-hostname

--- src/main/scala/moe/taiho/course_selection/cluster/HTTPMain.scala
+++ src/main/scala/moe/taiho/course_selection/cluster/HTTPMain.scala
@@ -111,9 +111,9 @@ object WebServer extends App {
 				}
 			}
 
-	val bindingFuture = Http().bindAndHandle(route, "localhost", 8000)
+	val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8000)
 
-	println(s"Server online at http://localhost:8000/\nPress RETURN to stop...")
+	println(s"Server online at http://0.0.0.0:8000/\nPress RETURN to stop...")
 	StdIn.readLine() // let it run until user presses return
 	bindingFuture
 		.flatMap(_.unbind()) // trigger unbinding from the port
