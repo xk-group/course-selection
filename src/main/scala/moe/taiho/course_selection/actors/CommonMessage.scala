125b6aba202bee40525b5100352c784bb2186173
Jianfeng Zhang <swordfeng123@gmail.com>
2017-08-20 03:34:43 +0800

    refactor

--- src/main/scala/moe/taiho/course_selection/actors/CommonMessage.scala
+++ src/main/scala/moe/taiho/course_selection/actors/CommonMessage.scala
@@ -3,5 +3,10 @@ package moe.taiho.course_selection.actors
 import moe.taiho.course_selection.KryoSerializable
 
 object CommonMessage {
-    trait Reason extends KryoSerializable {}
+    trait Reason extends KryoSerializable {
+        def message(): String = this.toString
+    }
+
+    case class Ping() extends KryoSerializable
+    case class Pong() extends KryoSerializable
 }
---
+++ src/main/scala/moe/taiho/course_selection/policies/CoursePolicy.scala
@@ -0,0 +1,37 @@
+package moe.taiho.course_selection.policies
+
+import moe.taiho.course_selection.KryoSerializable
+import moe.taiho.course_selection.actors.CommonMessage.Reason
+
+object CoursePolicy {
+    sealed trait Command extends KryoSerializable
+    case class Limit(num: Int) extends Command
+
+    case class CourseFull() extends Reason {
+        override def message(): String = "No available seats"
+    }
+}
+
+class CoursePolicy(id: Int) {
+    import CoursePolicy._
+
+    var numSelected: Int = 0
+    var limit: Int = 0
+
+    def validateTake(student: Int): Option[Reason] =
+        if (numSelected < limit) None else Some(CourseFull())
+
+    def take(student: Int): Unit = {
+        numSelected += 1
+    }
+
+    def drop(student: Int): Unit = {
+        numSelected -= 1
+    }
+
+    def setPolicy(cmd: Command): Unit = cmd match {
+        case Limit(num: Int) =>
+            limit = num
+    }
+
+}
